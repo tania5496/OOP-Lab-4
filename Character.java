87dacdaced71a7d9551aa39fd9004e42dfbe3a2e
tania5496 <87234112+tania5496@users.noreply.github.com>
2022-10-13 19:06:30 +0300

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,8 +1,27 @@
-public class Character {
-    int power;
-    int hp;
-//     - attributes: int power, in hp
-//   - methods: void kick(Character c), boolean isAlive()
+public abstract class Character {
+    private int power;
+    private int hp;
+    public void setHp(int newHp){
+        this.hp = newHp;
+    }
+    public Integer getHp(){
+        return hp;
+    }
+
+    public void setPower(int newPower){
+        this.power = newPower;
+    }
+    public Integer getPower(){
+        return power;
+    }
+
+    public Boolean isAlive(){
+        if (this.getHp() < 0){
+            return Boolean.FALSE;
+        }
+        else{
+            return Boolean.TRUE;
+        }
+    }
+    abstract public void kick(Character c);
 }
-
-

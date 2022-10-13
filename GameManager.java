926c18583851bf8d227a2cf05371e7e866388f8c
tania5496 <87234112+tania5496@users.noreply.github.com>
2022-10-13 19:13:29 +0300

    Update GameManager.java

--- GameManager.java
+++ GameManager.java
@@ -1,6 +1,15 @@
 public class GameManager{
-    // void fight(Character c1, Character c2){}
-//    to provide fight between to characters and explain via command
-//    line what happens during fight, till both of
-//    the characters are alive
+    void fight (Character c1, Character c2){
+        int power1 = c1.getPower();
+        int power2 = c2.getPower();
+        if (power1 == power2){
+            System.out.println("Those characters have equal power");
+        }
+        else if (power1 > power2) {
+            System.out.println("First character has more power than second.");
+        }
+        else{
+            System.out.println("Second character has more power than first.");
+        }
+    }
 }

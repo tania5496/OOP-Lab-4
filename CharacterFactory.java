40585275e2be9c9ca0d8ae3cf148200c7f83c491
tania5496 <87234112+tania5496@users.noreply.github.com>
2022-10-14 14:25:57 +0300

    Update CharacterFactory.java

--- CharacterFactory.java
+++ CharacterFactory.java
@@ -1,3 +1,14 @@
+import lotr.Character;
+import lotr.Elf;
+import lotr.Knight;
+import lotr.Hobbit;
+import lotr.King;
+import java.util.Random;
 public class CharacterFactory{
-    // Character createCharacter() {}
+     Character createCharacter() throws NoSuchMethodException, InstantiationException, IllegalAccessException{
+         Character[] arrayOfTypes = {new Elf(), new King(), new Knight(), new Hobbit()};
+          int rand = new Random().nextInt(arrayOfTypes.length);
+          Character c = arrayOfTypes[rand];
+          return c;
+     }
 }
